48a080a6cd00c8c220e0d0b78a53c17b267a930d
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2022-10-18 21:49:13 +0600

    add structure

--- games/brain-games.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import brainEven from './brain-even.js';
-import brainCalc from './brain-calc.js';
-
-brainEven();
-brainCalc();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,5 @@
 const greetings = () => {
   console.log('Welcome to the Brain Games!');
-  // const name = readlineSync.question('May I have your name? ');
-  // return `Hello, ${name}!`;
 };
 
 export default greetings;

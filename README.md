8d2afc3f87a7dc95b8d6c2b3342bbbee9f10034c
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2022-10-18 12:51:35 +0600

    add initial brain-gcd.js file

--- README.md
+++ README.md
@@ -20,6 +20,23 @@ Brain-even
 
 https://asciinema.org/a/6CofO9vrARWfIx9OzsR1sD1tq
 
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="asciinema-player.css" />
+</head>
+<body>
+  <div id="player"></div>
+  <script src="asciinema-player.min.js"></script>
+  <script>
+    AsciinemaPlayer.create(
+      '529506.cast',
+      document.getElementById('player'),
+      { cols: 80, rows: 24 }
+    );
+  </script>
+</body>
+</html>
+
 Brain-calc
 
 https://asciinema.org/a/5YuLGXjYv50ckZdgSaOr8PYHD
---
+++ games/brain-gcd.js
@@ -0,0 +1,30 @@
+import * as url from 'node:url';
+import runGame from '../src/index.js';
+
+const getRandomNumber = (min, max = 100) => Math.floor(min + Math.random() * (max + 1 - min));
+
+const description = 'Find the greatest common divisor of given numbers';
+
+const gcd = (a, b) => {
+  if (!b) {
+    return a;
+  }
+  return gcd(b, a % b);
+};
+
+const generateRound = () => {
+  const num1 = getRandomNumber(0, 100);
+  const num2 = getRandomNumber(0, 100);
+  const answer = String(gcd(num1, num2));
+  const question = `${num1} ${num2}`;
+  return [question, answer];
+};
+
+export default () => runGame(description, generateRound);
+
+if (import.meta.url.startsWith('file:')) {
+  const modulePath = url.fileURLToPath(import.meta.url);
+  if (process.argv[1] === modulePath) {
+    runGame(description, generateRound);
+  }
+}
